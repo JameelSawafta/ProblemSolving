b268fe472d7dbe40826a4e698264780ad01b4d86
JameelSawafta <jameel.wlar999@gmail.com>
2022-03-11 17:27:35 +0200

    HelpfulMaths

---
+++ src/com/company/HelpfulMaths.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HelpfulMaths {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String x=scanner.nextLine();
+        char[] y=x.toCharArray();
+        char[] z=new char[ y.length/2+1];
+        for (int i = 0; i < y.length-1; i+=2) {
+            z[i/2]=y[i];
+        }
+        z[ y.length/2]=y[y.length-1];
+        Arrays.sort(z);
+        for (int i = 0; i < z.length; i++) {
+            System.out.print(z[i]);
+            if (i!= z.length-1) System.out.print("+");
+        }
+
+    }
+}
